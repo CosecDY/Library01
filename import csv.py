7d9ba1642e4be8e22e56c44c2031d5d259f74b4b
popapideth <apidethpop555@gmail.com>
2023-10-10 23:42:13 +0700

    Create import csv.py

---
+++ import csv.py
@@ -0,0 +1,40 @@
+import csv
+
+def main():
+    while True:
+        print("1 : User")
+        print("2 : Admin")
+        print("3 : Exit")
+        choice = input("Choose : ")
+
+        if(choice == '1'):
+            print("---------------------------------")
+
+        elif(choice == '2'):
+            print("---------------------------------")
+     
+        elif(choice == '3'):
+            break
+
+
+def main_user():
+    while True:
+        print("1 : Login ")
+        print("2 : Register ")
+        print("3 : Forgot password ")
+        print("4 : Exit ")
+        choice = input("Choose : ")
+
+        if(choice == '1'):
+
+        elif(choice == '1'):
+            
+           
+    username = input("Username : ")
+    password = input("password : ")
+
+
+   
+def main_admin():
+    username = input("Username : ")    
+    password = input("password : ")        
